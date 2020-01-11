48d93fa773f296a1a509125ed46d88f1004cea20
Łukasz Szwałek <lukas.teodrx@gmail.com>
2020-01-11 11:51:59 +0100

    first

---
+++ src/Controller/PostController.php
@@ -0,0 +1,87 @@
+<?php
+namespace App\Controller;
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * @Route("/post",name="post.")
+ */
+class PostController extends AbstractController{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(PostRepository $postRepository) {
+        $posts=$postRepository->findAll();
+        //dump($posts);
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+            'posts'=>$posts,
+        ]);
+    }
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request) {
+        //create new post
+        $post = new Post();
+        //statyczne dodanie postu
+        //$post->setTitile('to bedzie tytuł');
+        $form=$this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        //$form->getErrors(); && $form->isValid()
+        if($form->isSubmitted() ){
+            $em= $this->getDoctrine()->getManager();
+            /**@var UploadedFile $file */
+            $file=$request->files->get('post')['attachment'];
+            dump($file);
+            if($file){
+                $filename= md5(uniqid()).'.'. $file->guessClientExtension();
+                $file->move(
+                    //TODO: get target directory, 
+                    $this->getParameter('uploads_dir'),
+                    $filename
+                );
+                $post->setImage($filename);
+            }
+            $em->persist($post);
+            $em->flush();
+            //dump($post);
+            return $this->redirect($this->generateUrl('post.index'));
+        }
+
+   
+       return $this->render('post/create.html.twig',[
+           'form'=> $form->createView()
+       ]);
+        // return $this->redirect($this->generateUrl('post.index'));
+       // return new Response('post was created');
+    }
+    /**
+     * @Route("/show/{id}", name="show")
+     * @return Response
+     */
+    public function show($id, PostRepository $postRepository)  {
+        $post=$postRepository->findPostWithCategory($id);
+        dump($post);
+        //$post=$postRepository->find($id);
+       // dump($post); die;
+        return $this->render('post/show.html.twig',[
+          'post'=> $post
+        ]);
+    }
+    /**
+     * @Route("/delete{id}", name="delete")
+     * @return Response
+     */
+    public function remove(Post $post) {
+        $em= $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+        $this->addFlash('success', 'Post was removed');
+        return $this->redirect($this->generateUrl('post.index'));
+    }
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Assert\NotBlank()
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titile;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $image;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="post")
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitile(): ?string
+    {
+        return $this->titile;
+    }
+
+    public function setTitile(string $titile): self
+    {
+        $this->titile = $titile;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
